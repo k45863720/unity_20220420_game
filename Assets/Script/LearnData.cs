e4bfd2975b0b4c3d216a7a52ee2daa5d02465ed3
k45863720 <103936853+k45863720@users.noreply.github.com>
2022-05-16 21:33:53 +0800

    彈珠軌跡跟資料四大類型

---
+++ Assets/Script/LearnData.cs
@@ -0,0 +1,31 @@
+//單行註解 標記，註記
+/*
+ * 多行註解
+ */
+// 藍色是保留字 白色是名稱 綠色是資料種類
+using UnityEngine;
+
+// 修飾詞 類別(藍圖) 類別名稱 繼承(物件導向程式 OOP 三大原則)
+public class LearnData : MonoBehaviour
+{
+          //LearnData 類別成員區域
+ //資料類型 欄位自訂名稱 結束符號
+ //  int    自訂名稱    ;
+    //欄位名稱 field
+ //欄位語法
+ int hp;  //定義一個整數資料 名稱叫做hp
+
+    //資料類型 四大基本類型
+    //整數 保存正負沒有小數點的資料 int
+    //浮點數 保存正負有小數點的資料 float
+    //字串 保存文字資料 string
+    //布林值 保存有、沒有資料 bool
+
+    int lv;
+    float exp;
+    string playName;
+    bool hasCureSkill;
+
+    // Clean code 無瑕的程式
+}
+//非LearnData 類別成員區域
