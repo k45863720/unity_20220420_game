abbb51f3266ebd3ee22129b2037e644338a49fba
k45863720 <103936853+k45863720@users.noreply.github.com>
2022-06-29 21:36:26 +0800

    1

--- Assets/Script/LearnStatic.cs
+++ Assets/Script/LearnStatic.cs
@@ -9,12 +9,16 @@ public class LearnStatic : MonoBehaviour
     {
         // 靜態屬性 static properties
         // 1. 取得 get
-        // 2. 取得靜態屬性語法:
+        // 取得靜態屬性語法:
         // 類別名稱.靜態屬性名稱
         print("隨機值:" + Random.value);
         print("螢幕寬度:" + Screen.width);
         print("圓周率:" + Mathf.PI);
 
         // 2. 設定 set (Read Only 不能設定)
+        // 設定靜態屬性語法:
+        // 類別名稱.靜態屬性名稱 指定 值;
+        Screen.brightness = 0.5f;
+        Cursor.visible = false;
     }
 }
